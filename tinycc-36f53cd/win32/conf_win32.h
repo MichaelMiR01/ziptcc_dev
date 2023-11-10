f3f0c3820bf0d95a11233d72042cf507d0bff861
MichaelMiR01 <project.draco@gmx.net>
2023-11-10 08:31:03 +0100

    Update to tinycc mob be8f894 2023-11-08

--- tinycc-36f53cd/win32/conf_win32.h
+++
@@ -1,6 +0,0 @@
-#ifdef _WIN32  
-#define CONFIG_TCC_SYSINCLUDEPATHS "{B}/include;{B}/include/stdinc;{B}/include/winapi;{B}/win32;{B}/win32/winapi;"
-#else
-#define CONFIG_TCC_SYSINCLUDEPATHS "{B}/include/stdinc/:{B}/include/"
-#endif
-
